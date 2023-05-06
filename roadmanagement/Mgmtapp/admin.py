3d4414d57fadb6de1de66aa542712abe354a1137
pinkal <pinkal@hanu.ai>
2023-05-06 22:05:13 +0530

    set repro

--- roadmanagement/Mgmtapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import SDO
-
-# Register your models here.
-admin.site.register(SDO)
--- roadmanagement/Mgmtapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MgmtappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Mgmtapp'
